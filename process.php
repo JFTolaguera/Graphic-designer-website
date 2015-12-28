237ba3f180018c7c5613071dffaf2ca368e44482
JF Tolaguera <jf.tolaguera@gmail.com>
2015-12-29 00:13:42 +0100

    update-28-12-15

---
+++ process.php
@@ -0,0 +1,153 @@
+<script>
+      function() {
+        if ("-ms-user-select" in document.documentElement.style && navigator.userAgent.match(/IEMobile/)) {
+          var msViewportStyle = document.createElement("style");
+          msViewportStyle.appendChild(
+            document.createTextNode("@-ms-viewport{width:auto!important}")
+          );
+          document.getElementsByTagName("head")[0].appendChild(msViewportStyle);
+      }})();
+
+</script>
+  <body id="process">
+    <nav>
+      <a href="<?= $currentUrl;?>index">About</a>
+      <a href="<?= $currentUrl;?>work">Work</a>
+      <a id="current" href="#">Process</a></nav>
+    <article class="flex">
+      <div class="section"><svg version="1.1" id="phase-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="136px" height="136px" viewBox="0 0 192 192" enable-background="new 0 0 192 192" xml:space="preserve">
+<path id="page-2" fill="#03A9F4" d="M170,45h-60c-3.3,0-6,2.7-6,6v90c0,3.3,2.7,6,6,6h60c3.3,0,6-2.7,6-6V51
+	C176,47.7,173.3,45,170,45z M114,57h32v3h-32V57z M163,69v3h-28v-3H163z M114,69h17v3h-17V69z M114,81h23v3h-23V81z M150,93v3h-23
+	v-3H150z M114,93h8v3h-8V93z M114,105h19v3h-19V105z M145,117v3h-16v-3H145z M114,117h11v3h-11V117z M130,132h-16v-3h16V132z
+	 M147,132h-13v-3h13V132z M147,108h-10v-3h10V108z M166,120h-17v-3h17V120z M165,105v3h-14v-3H165z M154,96v-3h11v3H154z M166,84
+	h-25v-3h25V84z"/>
+<g id="page-3">
+	<path id="XMLID_7_" fill="#03A9F4" d="M82,147H22c-3.3,0-6-2.7-6-6V51c0-3.3,2.7-6,6-6h60c3.3,0,6,2.7,6,6v90
+		C88,144.3,85.3,147,82,147z"/>
+	<path id="XMLID_29_" fill="#FFFFFF" d="M52,123c-5,0-9,4-9,9c0,5,4,9,9,9s9-4,9-9C61,127,57,123,52,123z M52,138c-3.3,0-6-2.7-6-6
+		s2.7-6,6-6s6,2.7,6,6S55.3,138,52,138z"/>
+	<path id="XMLID_11_" fill="#FFFFFF" d="M52,99c-5,0-9,4-9,9c0,5,4,9,9,9s9-4,9-9C61,103,57,99,52,99z M52,114c-3.3,0-6-2.7-6-6
+		s2.7-6,6-6s6,2.7,6,6S55.3,114,52,114z"/>
+	<path id="XMLID_22_" fill="#FFFFFF" d="M28,99c-5,0-9,4-9,9c0,5,4,9,9,9s9-4,9-9C37,103,33,99,28,99z M28,114c-3.3,0-6-2.7-6-6
+		s2.7-6,6-6s6,2.7,6,6S31.3,114,28,114z"/>
+	<path id="XMLID_25_" fill="#FFFFFF" d="M28,75c-5,0-9,4-9,9c0,5,4,9,9,9s9-4,9-9C37,79,33,75,28,75z M28,90c-3.3,0-6-2.7-6-6
+		s2.7-6,6-6s6,2.7,6,6S31.3,90,28,90z"/>
+	<path id="XMLID_16_" fill="#FFFFFF" d="M52,75c-5,0-9,4-9,9c0,5,4,9,9,9s9-4,9-9C61,79,57,75,52,75z M52,90c-3.3,0-6-2.7-6-6
+		s2.7-6,6-6s6,2.7,6,6S55.3,90,52,90z"/>
+	<path id="XMLID_48_" fill="#FFFFFF" d="M76,75c-5,0-9,4-9,9c0,5,4,9,9,9s9-4,9-9C85,79,81,75,76,75z M76,90c-3.3,0-6-2.7-6-6
+		s2.7-6,6-6s6,2.7,6,6S79.3,90,76,90z"/>
+	<path id="XMLID_19_" fill="#FFFFFF" d="M52,51c-5,0-9,4-9,9c0,5,4,9,9,9s9-4,9-9C61,55,57,51,52,51z M52,66c-3.3,0-6-2.7-6-6
+		s2.7-6,6-6s6,2.7,6,6S55.3,66,52,66z"/>
+	<line id="XMLID_51_" fill="none" stroke="#FFFFFF" stroke-width="4" stroke-miterlimit="10" x1="57" y1="103" x2="71" y2="89"/>
+
+		<line id="XMLID_52_" fill="#FFFFFF" stroke="#FFFFFF" stroke-width="4" stroke-miterlimit="10" x1="52" y1="116" x2="52" y2="124"/>
+
+		<line id="XMLID_53_" fill="#FFFFFF" stroke="#FFFFFF" stroke-width="4" stroke-miterlimit="10" x1="46" y1="128" x2="32" y2="114"/>
+	<line id="XMLID_54_" fill="#FFFFFF" stroke="#FFFFFF" stroke-width="4" stroke-miterlimit="10" x1="52" y1="100" x2="52" y2="92"/>
+	<line id="XMLID_55_" fill="#FFFFFF" stroke="#FFFFFF" stroke-width="4" stroke-miterlimit="10" x1="52" y1="76" x2="52" y2="68"/>
+	<line id="XMLID_56_" fill="#FFFFFF" stroke="#FFFFFF" stroke-width="4" stroke-miterlimit="10" x1="28" y1="100" x2="28" y2="92"/>
+	<line id="XMLID_57_" fill="#FFFFFF" stroke="#FFFFFF" stroke-width="4" stroke-miterlimit="10" x1="33" y1="78" x2="46" y2="65"/>
+	<line id="XMLID_58_" fill="#FFFFFF" stroke="#FFFFFF" stroke-width="4" stroke-miterlimit="10" x1="71" y1="78" x2="58" y2="65"/>
+</g>
+<g id="page-1">
+	<linearGradient id="ombre_1_" gradientUnits="userSpaceOnUse" x1="16" y1="96" x2="30" y2="96">
+		<stop  offset="0" style="stop-color:#000000;stop-opacity:0"/>
+		<stop  offset="1" style="stop-color:#000000;stop-opacity:0.2"/>
+	</linearGradient>
+	<path id="ombre" fill="url(#ombre_1_)" d="M30,147h-8c-3.3,0-6-2.7-6-6V51c0-3.3,2.7-6,6-6h8V147z"/>
+	<path id="XMLID_8_" fill="#03A9F4" d="M90,147H30c-3.3,0-6-2.7-6-6V51c0-3.3,2.7-6,6-6h60c3.3,0,6,2.7,6,6v90
+		C96,144.3,93.3,147,90,147z"/>
+	<path id="_x31_" fill="#FFFFFF" d="M60,76H48v8h8v24h-8v8h24v-8h-8V80C64,77.8,62.2,76,60,76z"/>
+</g>
+</svg>
+
+        <h3>Project definition and contract</h3>
+        <p>Via the form embedded in the about page, tell me the maximum about your project. Android design ? Web development ? The more info I get, the more efficient it will run. Explain me your ideas, and what will be my part of the job.</br><span class="soft">Info I might need : Aim for this project ? Target audience, planning, competition ?</span></br>Then I draw a quotation, and send it to you by mail. If you accept the estimation, please sign and return it to me.</p>
+      </div>
+      <div class="arrow"><svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="32px" height="32px" viewBox="0 0 32 32" enable-background="new 0 0 32 32" xml:space="preserve">
+<path id="XMLID_2_" d="M16,8l-1.4,1.4l5.6,5.6H8v2h12.2l-5.6,5.6L16,24l8-8L16,8z"/>
+</svg>
+
+      </div>
+      <div class="section"><svg version="1.1" id="phase-2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="136px" height="136px" viewBox="0 0 192 192" enable-background="new 0 0 192 192" xml:space="preserve">
+<path id="base" fill="#03A9F4" d="M156,162H36c-3.3,0-6-2.7-6-6V36c0-3.3,2.7-6,6-6h120c3.3,0,6,2.7,6,6v120
+	C162,159.3,159.3,162,156,162z"/>
+<line id="XMLID_22_" opacity="0.6" fill="none" stroke="#AED6E5" stroke-width="2" stroke-miterlimit="10" x1="96" y1="30" x2="96" y2="162"/>
+<line id="XMLID_17_" opacity="0.6" fill="none" stroke="#AED6E5" stroke-width="2" stroke-miterlimit="10" x1="118" y1="30" x2="118" y2="162"/>
+<line id="XMLID_21_" opacity="0.6" fill="none" stroke="#AED6E5" stroke-width="2" stroke-miterlimit="10" x1="52" y1="30" x2="52" y2="162"/>
+<line id="XMLID_8_" opacity="0.6" fill="none" stroke="#AED6E5" stroke-width="2" stroke-miterlimit="10" x1="74" y1="30" x2="74" y2="162"/>
+<line id="XMLID_18_" opacity="0.6" fill="none" stroke="#AED6E5" stroke-width="2" stroke-miterlimit="10" x1="140" y1="30" x2="140" y2="162"/>
+<line id="XMLID_15_" opacity="0.6" fill="none" stroke="#AED6E5" stroke-width="2" stroke-miterlimit="10" x1="162" y1="97" x2="30" y2="97"/>
+<line id="XMLID_14_" opacity="0.6" fill="none" stroke="#AED6E5" stroke-width="2" stroke-miterlimit="10" x1="162" y1="119" x2="30" y2="119"/>
+<line id="XMLID_13_" opacity="0.6" fill="none" stroke="#AED6E5" stroke-width="2" stroke-miterlimit="10" x1="162" y1="53" x2="30" y2="53"/>
+<line id="XMLID_12_" opacity="0.6" fill="none" stroke="#AED6E5" stroke-width="2" stroke-miterlimit="10" x1="162" y1="75" x2="30" y2="75"/>
+<line id="XMLID_11_" opacity="0.6" fill="none" stroke="#AED6E5" stroke-width="2" stroke-miterlimit="10" x1="162" y1="141" x2="30" y2="141"/>
+<path id="_x32_" fill="#FFFFFF" d="M145,77l2,8h-24V69c0-4.4,3.6-8,8-8h8v-8h-14l-2-8h16c4.4,0,8,3.6,8,8v8c0,4.4-3.6,8-8,8h-8v8
+	H145z"/>
+</svg>
+
+        <h3>Mock-ups, review and approval cycle</h3>
+        <p>I always draw mockups on my sketchbook first, then design it on my graphic software/code editor, following your specifications. We set off a phase of reviewing, approving, fine tuning the mockup-ups until both you and me are satisfied by the design.</br><span class="soft">Main deliverables : written ideas, rough sketches, documented site map, wireframes, visual mock-ups, some lines of code for the basic design. For websites, I need all of the content provided by the client.</span></p>
+      </div>
+      <div class="arrow"><svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="32px" height="32px" viewBox="0 0 32 32" enable-background="new 0 0 32 32" xml:space="preserve">
+<path id="XMLID_2_" d="M16,8l-1.4,1.4l5.6,5.6H8v2h12.2l-5.6,5.6L16,24l8-8L16,8z"/>
+</svg>
+
+      </div>
+      <div class="section"><svg version="1.1" id="phase-3" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="136px" height="136px" viewBox="0 0 192 192" enable-background="new 0 0 192 192" xml:space="preserve">
+<polyline id="câbles-grue" fill="none" stroke="#03A9F4" stroke-width="2" stroke-miterlimit="10" points="50,42 129,27 130,27
+	162,46.8 "/>
+<path id="grue" fill="#03A9F4" d="M164,42h-4c-1.1,0-2,0.9-2,2c0-1.1-0.9-2-2-2h-4c-1.1,0-2,0.9-2,2v2h-11.8c-0.8,0-1.5,0.4-1.8,1.1
+	L135,50l-4-24h-3l-4,24l-9-9H38L26,53v3h98l1,3h-3v85.1l-1.5,1.9H109c-1.1,0-2,0.9-2,2v4c0,1.1,0.9,2,2,2c-1.1,0-2,0.9-2,2v4
+	c0,1.1,0.9,2,2,2h2h37h2c1.1,0,2-0.9,2-2v-4c0-1.1-0.9-2-2-2c1.1,0,2-0.9,2-2v-4c0-1.1-0.9-2-2-2h-11.5l-1.5-1.9V59h-3l1-3h15v12
+	c0,1.1,0.9,2,2,2h4c1.1,0,2-0.9,2-2c0,1.1,0.9,2,2,2h4c1.1,0,2-0.9,2-2V56v-7v-5C166,42.9,165.1,42,164,42z M39,53h-7l7-7V53z
+	 M42,53v-7l7,7H42z M51,51l-7-7h7V51z M54,44h7l-7,7V44z M63,53h-7l7-7V53z M66,53v-7l7,7H66z M75,51l-7-7h7V51z M78,44h7l-7,7V44z
+	 M87,53h-7l7-7V53z M90,53v-7l7,7H90z M99,51l-7-7h7V51z M102,44h7l-7,7V44z M111,53h-7l7-7V53z M114,53v-7l7,7H114z M129.6,34.4
+	l1.8,12.6h-3.6L129.6,34.4z M125,64l7,7h-7V64z M125,74h7l-7,7V74z M125,88l7,7h-7V88z M125,98h7l-7,7V98z M125,112l7,7h-7V112z
+	 M125,122h7l-7,7V122z M125,143v-7l7,7H125z M134,141l-7-7h7V141z M134,131h-7l7-7V131z M134,117l-7-7h7V117z M134,107h-7l7-7V107z
+	 M134,93l-7-7h7V93z M134,83h-7l7-7V83z M134,69l-7-7h7V69z M145,53h-8v-1.1c0-0.6,0.1-1.2,0.4-1.8L138,49h7V53z M147,53v-4h3v4H147
+	z"/>
+<g id="levage">
+	<polygon id="moufle" fill="#03A9F4" points="49,69 49,75 45,75 45,78 51,85 51,87 55,87 55,85 61,78 61,75 57,75 57,69 	"/>
+
+		<line id="câble_droite" fill="#03A9F4" stroke="#03A9F4" stroke-width="2" stroke-miterlimit="10" x1="44" y1="60" x2="50" y2="72"/>
+
+		<line id="câble_gauche" fill="#03A9F4" stroke="#03A9F4" stroke-width="2" stroke-miterlimit="10" x1="62" y1="60" x2="56" y2="72"/>
+	<path id="système" fill="#03A9F4" d="M62,52v1H44v-1h-3v9c0,1.1,0.9,2,2,2h1v-1h18v1h1c1.1,0,2-0.9,2-2v-9H62z M62,59H44v-3h18V59
+		z"/>
+	<path id="prémur" fill="#03A9F4" d="M67,135H39c-1.7,0-3-1.3-3-3V90c0-1.7,1.3-3,3-3h28c1.7,0,3,1.3,3,3v42
+		C70,133.7,68.7,135,67,135z"/>
+	<path id="_x33_" fill="#FFFFFF" d="M41,123v8c0,0,11.5,0,16,0s8-3.7,8-8c0-1.6,0-3.6,0-7c0-2-1.7-5-4-5c2.3,0,4-3,4-5
+		c0-3.4,0-5.4,0-7c0-4.5-3.5-8-8-8s-16,0-16,0v8h16v8H47l-2,8h12v8H41z"/>
+</g>
+</svg>
+
+        <h3>Development, creation</h3>
+        <p>This is typically the main phase. After approval of the mock-ups, I design, code, create all the deliverables, meeting the deadlines. Regularly, I share a progress report and comments about the work. When work is almost finished, this is a time for a full review.</p>
+      </div>
+      <div class="arrow"><svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="32px" height="32px" viewBox="0 0 32 32" enable-background="new 0 0 32 32" xml:space="preserve">
+<path id="XMLID_2_" d="M16,8l-1.4,1.4l5.6,5.6H8v2h12.2l-5.6,5.6L16,24l8-8L16,8z"/>
+</svg>
+
+      </div>
+      <div class="section"><svg version="1.1" id="phase-4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="136px" height="136px" viewBox="0 0 192 192" enable-background="new 0 0 192 192" xml:space="preserve">
+<path id="maison" fill="#03A9F4" d="M161.6,47.1c-1.1-1.4-2.8-2.1-4.6-2.1H74v-7h2v-5H63v5h2v7H52v-7h2v-5H41v5h2l0,7h-8.4
+	c-1.8,0-3.4,0.8-4.6,2.1L18.4,61.6C16.8,63.4,16,65.6,16,68v91h160V68c0-2.4-0.8-4.6-2.4-6.4L161.6,47.1z M128,74c0-1.1,0.9-2,2-2
+	h12c1.1,0,2,0.9,2,2v26h-16V74z M84,120c0-1.1,0.9-2,2-2h20c1.1,0,2,0.9,2,2v27H84V120z M82,149h28v3H82V149z M48,74
+	c0-1.1,0.9-2,2-2h12c1.1,0,2,0.9,2,2v26H48V74z M67,145H45v-3h22V145z M48,140v-20c0-1.1,0.9-2,2-2h12c1.1,0,2,0.9,2,2v20H48z
+	 M67,105H45v-3h22V105z M112,157H80v-3h32V157z M147,145h-22v-3h22V145z M128,140v-20c0-1.1,0.9-2,2-2h12c1.1,0,2,0.9,2,2v20H128z
+	 M147,105h-22v-3h22V105z"/>
+<path id="_x34_" fill="#FFFFFF" d="M92,58h-8v21c0,4.4,3.6,8,8,8h4v11l8-2v-9h4v-8h-4v-8h-8v8h-4L92,58z"/>
+</svg>
+
+        <h3>Delivery, payment and maintenance</h3>
+        <p>I send you all drawables, icons or complete site structure with HTML, CSS and JS files for websites. If needed, I can make some polishing or small improvements. When everything is ok, I send you the invoice. </br><span class="soft">Payment by : PayPal, bank transfer (details on request) or cheque payable to Jean-François Tolaguera.</span></p>
+      </div>
+    </article>
+    <article>
+      <h1>FAQs</br></h1>
+      <p><b>Can you do CMS development ?</b></br>No, I'm a front-end developer and web designer. I create mockups, and convert these into responsive websites. I don't handle the launching of the site online.</p>
+      <p><b>How do we communicate ?</b></br>Mostly with mails, hangouts or skype. I travel a lot, I don't have a standing phone line.</p>
+      <h1>Rates & fixed prices</h1>
+      <p>Usually my rates depends on the project and the amount of work. When we discuss about the project, make sure to tell me the maximum, about it's scope and details (we can schedule a phone call, hangout or skype if needed), so that I can assess a quote. Although, I have some fixed prices for some works I know well.</br></p><b>Android Product icon : </span><span class="price-tag">99€</span></br>Android feature or promotionnal graphic (for Play Store) : <span class="price-tag">29€</span>
+    </article>
